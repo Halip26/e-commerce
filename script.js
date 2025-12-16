aaccab9d1f9dd82fa526a76a3a69c53c46a185f3
Halip26 <77138807+Halip26@users.noreply.github.com>
2025-12-16 21:10:00 +0800

    rearreng of js codes

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
-  const menuBtn = document.getElementById("menuBtn");
-  const sideNav = document.getElementById("sideNav");
-  const menu = document.getElementById("menu");
+// make menu burger button works
+const menuBtn = document.getElementById("menuBtn");
+const sideNav = document.getElementById("sideNav");
+const menu = document.getElementById("menu");
 
-  sideNav.style.right = "-250px";
+sideNav.style.right = "-250px";
 
-  function toggleMenu() {
-    if (sideNav.style.right === "-250px") {
-      sideNav.style.right = "0";
-      menu.src = "images/close.png";
-    } else {
-      sideNav.style.right = "-250px";
-      menu.src = "images/menu.png";
-    }
+function toggleMenu() {
+  if (sideNav.style.right === "-250px") {
+    sideNav.style.right = "0";
+    menu.src = "images/close.png";
+  } else {
+    sideNav.style.right = "-250px";
+    menu.src = "images/menu.png";
   }
+}
 
-  menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
 
-  const text = document.getElementById("text");
-  let i = 0;
-  const t = "Give yourself a new style this year";
+// typing effect
+const text = document.getElementById("text");
+let i = 0;
+const t = "Give yourself a new style this year";
 
-  function typing() {
-    if (i < t.length) {
-      text.innerHTML += t.charAt(i);
-      i++;
-      setTimeout(typing, 150);
-    } else {
-      i = 0;
-      text.innerHTML = " ";
-      setTimeout(typing, 200);
-    }
+function typing() {
+  if (i < t.length) {
+    text.innerHTML += t.charAt(i);
+    i++;
+    setTimeout(typing, 150);
+  } else {
+    i = 0;
+    text.innerHTML = " ";
+    setTimeout(typing, 200);
   }
+}
 
-  typing();
+typing();
 
-  // open the img in a new window
-  function openImg(element) {
-    window.open(element.src);
-  }
+// open the img in a new window
+function openImg(element) {
+  window.open(element.src);
+}
 
-  // for zoomin effect when click
-  function zoomIn(element) {
-    element.classList.toggle("zoom-in");
-  }
+// for zoomin effect when click
+function zoomIn(element) {
+  element.classList.toggle("zoom-in");
+}
 
-  // Get the current year
-  const currentYear = new Date().getFullYear();
+// Get the current year
+const currentYear = new Date().getFullYear();
 
-  // Set the current year to the span with id "currentYear"
-  document.getElementById("currentYear").textContent = currentYear;
+// Set the current year to the span with id "currentYear"
+document.getElementById("currentYear").textContent = currentYear;
